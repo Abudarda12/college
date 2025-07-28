c9246c07035c5cc048d2fdcb8d0d5fe986be2c60
rishiraj526 <rishiraj00346@gmail.com>
2025-07-29 02:30:33 +0530

    set env

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ app.use('/', authRoutes);
 
 
 // Start server
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
